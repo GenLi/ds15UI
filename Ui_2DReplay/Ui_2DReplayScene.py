83a1d0f88239a9a68d54182deeaf46c1e0291b90
walkerning <foxdoraame@gmail.com>
2013-07-16 01:04:25 +0800

    在2DReplay中加入了简单的动画实现

--- Ui_2DReplay/Ui_2DReplayScene.py
+++ Ui_2DReplay/Ui_2DReplayScene.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-#Ver 0.1 edited at 2013-07-14-15:29
-#Changes: soldiers' display(not completed)
+#Ver 0.3 edited at 2013-07-16-0:48
+#Changes: solves the bug in moving animation
+#         attack animation added
 
 #scene, view of replay
 
 from Ui_Units import *
-import sys
+import sys, math
 
 
 class Unit_forTest:
@@ -20,6 +21,7 @@ maps = [[0, 0, 0, 1, 1, 1, 0, 1],
         [3, 3, 0, 1, 2, 2, 2, 2],
         [0, 0, 0, 0, 2, 2, 2, 2]]
 units = [a(0, 0, LANCER), a(1, 3, SABER), a(2, 3, ARCHER), a(3, 4, HERO_1)]
+route = ((0, 0), (0, 1), (1, 1), (1, 2), (1, 3))
 #data for test
 
 
@@ -48,14 +50,69 @@ class Ui_ReplayView(QtGui.QGraphicsView):
         self.SetSoldiers(units)
         #initialization of soldier units
         #initialization of the cursor
+        self.timeline = QtCore.QTimeLine()
+        self.animation = QtGui.QGraphicsItemAnimation()
+        #animation
     
     #def SetSoliders(self):
     def SetSoldiers(self, units):
         "set the pos of soldiers"
+        #what if a soldier dies??
         for i in range(len(units)):
             self.soldierItem[i].setPos(GetPos(units[i].x, units[i].y))
             self.soldierItem[i].mapX, self.soldierItem[i].mapY = \
                                       units[i].x, units[i].y
+
+    #def MovingAnimation(self):
+    def MovingAnimation(self, idnum, route):
+        "moving animation, displayed when the soldier moves"
+        TIME_PER_FRAME = 1000#ms, one-step movement in a frame
+        FRAMES_BEFORE_MOVE = 3
+        soldier = self.soldierItem[idnum]
+        #self.setPos(GetPos(route[0].x, route[0].y))
+        soldier.setPos(GetPos(route[0][0], route[0][1]))
+        steps = len(route)-1
+        frames = steps+FRAMES_BEFORE_MOVE
+
+        self.timeline = QtCore.QTimeLine(frames*TIME_PER_FRAME)
+        self.timeline.setCurveShape(self.timeline.LinearCurve)
+        self.animation = QtGui.QGraphicsItemAnimation()
+        self.animation.setItem(soldier)
+        self.animation.setTimeLine(self.timeline)
+        for i in range(steps+1):
+            #
+            pos = GetPos(route[i][0], route[i][1])
+            self.animation.setPosAt(float((i+FRAMES_BEFORE_MOVE))/frames, pos)
+        #
+        soldier.SetMapPos(route[steps][0], route[steps][1])
+        #self.animation.setPosAt(1, soldier.GetPos())
+        self.timeline.start()
+
+    #def AttackAnimation(self):
+    def AttackAnimation(self, selfId, targetId, damage, info = ""):
+        "attack animation, displayed when the soldier launches an attack."
+        TOTAL_TIME = 2000
+        TIME_FOR_MOVING = 500
+        TIME_WHEN_RESETING = 1960
+        DIST = 0.3
+        attacker = self.soldierItem[selfId]
+        target = self.soldierItem[targetId]
+        
+        self.timeline = QtCore.QTimeLine(TOTAL_TIME)
+        self.timeline.setCurveShape(self.timeline.LinearCurve)
+        self.animation = QtGui.QGraphicsItemAnimation()
+        self.animation.setItem(attacker)
+        self.animation.setTimeLine(self.timeline)
+        r = DIST/math.sqrt((attacker.mapX-target.mapX)**2+(attacker.mapY-target.mapY)**2)
+        pos = attacker.GetPos()*(1-r)+target.GetPos()*r
+        self.animation.setPosAt(0, attacker.GetPos())
+        self.animation.setPosAt(float(TIME_FOR_MOVING)/TOTAL_TIME, pos)
+        self.animation.setPosAt(float(TIME_WHEN_RESETING)/TOTAL_TIME, pos)
+        self.animation.setPosAt(1, attacker.GetPos())
+        #show damage and info
+        self.timeline.start()
+
+    #def DieAnimation(self, selfId):
 
 
 
@@ -66,4 +123,7 @@ if __name__=="__main__":
     view.setBackgroundBrush(QtGui.QColor(255, 255, 255))
     view.setWindowTitle("Replay")
     view.show()
+    #view.MovingAnimation(1, route)#for test
+    print view.soldierItem[1].mapX, view.soldierItem[1].mapY#for test
+    view.AttackAnimation(0, 3, 0)
     sys.exit(app.exec_())
--- Ui_2DReplay/Ui_Units.py
+++ Ui_2DReplay/Ui_Units.py
@@ -1,25 +1,54 @@
-#回放界面、地图编辑器界面场景所需图元
-#地图格点：
-#单位：
-#光标：
+# -*- coding: utf-8 -*-
+#Ver 0.3 edited at 2013-07-16-0:50
+#Changes: some unnecessary contents deleted
+
+#items needed in replay scene
+#grids of map
+#soldiers
+#cursor
 
 from PyQt4 import QtGui, QtCore
+from basic import *
+
+TRAP_TRIGGERED = 8
 
 
 
-UNIT_WIDTH = 50
-UNIT_HEIGHT = 50
+UNIT_WIDTH = 100
+UNIT_HEIGHT = 100
 PEN_WIDTH = 0.5
+
+def GetPos(mapX, mapY):
+    return QtCore.QPointF(mapX*UNIT_WIDTH, mapY*UNIT_HEIGHT)
 
 class Ui_MapUnit(QtGui.QGraphicsItem):
     "the unit of the map. Generalized."
-    def __init__(self, x, y, terrain):
+    def __init__(self, x, y, terrain, parent = None):
         "Initialize the flags and position info"
+        QtGui.QGraphicsItem.__init__(self, parent)
         self.mapX = x
         self.mapY = y
+        self.terrain = terrain
         #load pixmap
         self.selected = False
         self.coverColor = None
+
+    #def GetImage(self, painter, image):
+    def GetPainter(self, painter):
+        "change the painter according to the terrain, selection state, etc"
+        terrainColor = {PLAIN:QtGui.QColor(205, 173, 0), MOUNTAIN:QtGui.QColor(139, 105, 20),
+                        FOREST:QtGui.QColor(0, 139, 0), BARRIER:QtGui.QColor(139, 126, 102),
+                        TURRET:QtGui.QColor(255, 0, 0), TRAP:QtGui.QColor(139, 0, 0),
+                        TEMPLE:QtGui.QColor(0, 0, 139), GEAR:QtGui.QColor(139, 101, 8)}
+        brush = QtGui.QBrush()
+        brush.setColor(terrainColor[self.terrain])
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        pen = QtGui.QPen()
+        pen.setColor(QtGui.QColor(0, 0, 0))
+        pen.setStyle(QtCore.Qt.DotLine)
+        painter.setPen(pen)
+        painter.setBrush(brush)
+        #for test
 
     def boundingRect(self):
         return QtCore.QRectF(0-PEN_WIDTH, 0-PEN_WIDTH,
@@ -28,27 +57,47 @@ class Ui_MapUnit(QtGui.QGraphicsItem):
 
     def paint(self, painter, option, widget):
         #draw pixmap
-        pen = QtGui.QPen()
-        pen.setColor(QtGui.QColor(0, 0, 0))
-        pen.setStyle(Qt.DotLine)
-        brush = QtGui.QBrush()
-        brush.setColor(QtGui.QColor(255, 0, 0))
-        brush.setStyle(Qt.SolidPattern)
-        painter.setPen(pen)
-        painter.setBrush(brush)
-        painter.drawRect()
+        self.GetPainter(painter)
+        painter.drawRect(QtCore.QRect(0, 0, UNIT_WIDTH, UNIT_HEIGHT))
         #for test
 
 
-class Ui_SoldierUnit(QtGuiQGraphicsItem):
+class Ui_SoldierUnit(QtGui.QGraphicsItem):
     "the unit of the soldiers. Generalized."
-    def __init__(self, x, y, soldiertype):
-        pass
+    #def __init__(self, units):
+    def __init__(self, x, y, soldiertype, parent = None):
+        "initialize the flags and map info"
+        QtGui.QGraphicsItem.__init__(self, parent)
+        self.mapX = x
+        self.mapY = y
+        self.type = soldiertype
+        self.selected = False
+
+    def SetMapPos(self, x, y):
+        self.mapX = x
+        self.mapY = y
+
+    def GetPos(self):
+        return GetPos(self.mapX, self.mapY)
 
     def boundingRect(self):
-        pass
+        return QtCore.QRectF(0-PEN_WIDTH, 0-PEN_WIDTH,
+                             UNIT_WIDTH+PEN_WIDTH, UNIT_HEIGHT+PEN_WIDTH)
+        #regard the upleft corner as origin
 
-    def paint(self):
-        pass
+    def paint(self, painter, option, widget):
+        imageRoute = {SABER:"saber.png",
+                     LANCER:"lancer.png",
+                     ARCHER:"archer.png",
+                     DRAGON_RIDER:"dragonrider.png",
+                     WARRIOR:"warrior.png",
+                     WIZARD:"wizard.png",
+                     HERO_1:"hero1.png"}
+        fileRoute = "SoldierImage\\"
+        image = QtGui.QImage(fileRoute+imageRoute[self.type])
+        painter.setCompositionMode(painter.CompositionMode_Multiply)
+        painter.drawImage(QtCore.QRectF(0, 0, UNIT_WIDTH, UNIT_HEIGHT), image)
+
+
 
 
